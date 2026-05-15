f897f413dce06b41c6379522dc4a6c26ca692770
satyam <satyamkumar040721@gmail.com>
2026-05-15 14:20:23 +0530

    Added square pattern

---
+++ Pattern/square.java
@@ -0,0 +1,20 @@
+package Pattern;
+import java.util.Scanner;
+public class square {
+
+    public void squarePattern(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    square s = new square();
+
+        s.squarePattern(n);
+}
+}
