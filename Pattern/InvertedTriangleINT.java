6bdb1801e5c29323026e0448b0faaa27e2abf7e9
satyam <satyamkumar040721@gmail.com>
2026-05-28 15:58:06 +0530

    inverted with numbers

---
+++ Pattern/InvertedTriangleINT.java
@@ -0,0 +1,29 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class InvertedTriangleINT {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+}
+}
+
+
+/*
+--------------------
+1234567
+123456
+12345
+1234
+123
+12
+1
+---------------------
+*/
