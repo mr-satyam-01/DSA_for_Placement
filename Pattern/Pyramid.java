6ea3b9ac9a2aeb050eeba786594600a1ca7628c2
satyam <satyamkumar040721@gmail.com>
2026-05-28 17:25:22 +0530

    the pyramid code

--- Pattern/Pyramid.java
+++ Pattern/Pyramid.java
@@ -1,5 +1,35 @@
 package Pattern;
 
+import java.util.Scanner;
+
 public class Pyramid {
-    
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+for(int i=0; i<n; i++){
+    for(int j=0; j<n-i-1; j++){
+        System.out.print(" ");
+    }
+    for(int j=0; j<i*2-1; j++){
+        System.out.print("*");
+    }
+    for(int j=0; j<n-i-1; j++){
+        System.out.print(" ");
+    }
+
+    System.out.println();
 }
+}
+}
+
+
+
+
+/*
+-------------------------
+   *
+  ***
+ *****
+*******
+----------------------
+ */
